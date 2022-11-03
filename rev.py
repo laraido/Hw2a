b35848d8638ace22531cc46a62619e258583b445
laraido <116837728+laraido@users.noreply.github.com>
2022-11-03 09:31:40 -0700

    Update rev.py

--- rev.py
+++ rev.py
@@ -1,6 +1,16 @@
 def reverse_list(userlist):
-	userlist = userlist[::-1]
-	return userlist
-	
-#userlist = [1,2,3,4,5]
-#print(reverse_list(userlist))
+    if len(userlist)==0:
+        return none
+    else:
+        a = []
+        list_size = len(userlist)-1
+        for i in range(list_size, -1, -1):
+            a.append(userlist[i])
+            
+        return a
+"""
+userlist = [1, 2, 3, 4, 5]
+    
+reverse_list(userlist)
+print(reverse_list(userlist))
+"""
